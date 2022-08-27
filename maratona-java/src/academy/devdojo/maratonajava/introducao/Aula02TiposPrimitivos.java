5c1d74911d7546268b06b64bd75f8c9ca4235477
Alex Júnior <alexjunior651@hotmail.com>
2022-08-27 15:57:00 -0300

    Utilizando o casting

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,10 +3,10 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long boolean
-        int idade = 10;
+        int idade = (int)1000000L;
         long numeroGrande = 1000000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.0F;
         byte idadeByte = 10;
         short idadeShort = 10;
         boolean verdadeiro = true;
