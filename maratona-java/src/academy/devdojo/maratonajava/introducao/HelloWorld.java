d1610735d1cf322ffd77df4af951c489d5d29187
Alex Júnior <alexjunior651@hotmail.com>
2022-08-27 15:26:20 -0300

    Comentários em Java

--- maratona-java/src/academy/devdojo/maratonajava/introducao/HelloWorld.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/HelloWorld.java
@@ -1,6 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
 public class HelloWorld {
+    //Isso é um comentário de uma linha
+    /*
+    Isso é um comentário
+    de mútiplas linhas
+     */
     public static void main(String[] args) {
         System.out.println("Wello Wolrd Java");
     }
