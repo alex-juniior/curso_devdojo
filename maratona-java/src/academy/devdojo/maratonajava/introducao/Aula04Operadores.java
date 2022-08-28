bfdc8a4f7dc94040d07940a790fad92b4ed5283d
Alex Júnior <alexjunior651@hotmail.com>
2022-08-28 00:17:31 -0300

    Operadores de igualdade, relacionais e precedência

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -7,5 +7,47 @@ public class Aula04Operadores {
         int numero02 = 20;
         int resultado = numero01 * numero02;
         System.out.println(resultado);
+
+        // %
+        int resto = 21 % 2;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 <= 20;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+
+        // && (AND) || (or) !
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade <30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalCOntaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 500F;
+        boolean isPlaystationCincoCompravel = valorTotalCOntaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+
+        System.out.println("isPlaystationCincoCompravel " +isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus+= 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        //
+        int contador = 0;
+        contador +=1;
+        contador ++;
+        contador --;
+        ++contador;
+        --contador;
+        System.out.println(contador);
     }
 }
