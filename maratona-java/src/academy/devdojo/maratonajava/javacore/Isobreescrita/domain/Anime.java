739d286482b46f5737410c1b15de232534be5d87
Alex Júnior <alexjunior651@hotmail.com>
2022-09-19 00:36:19 -0300

    Sobreescrita método toString

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Isobreescrita/domain/Anime.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Isobreescrita.domain;
+
+public class Anime {
+    private String nome;
+
+    @Override
+    public String toString() {
+        return "Anime: " + this.nome;
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
