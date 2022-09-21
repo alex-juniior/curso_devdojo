c34d5d54a51d6233d6b628f2f4d5fcaab9d7b720
Alex Júnior <alexjunior651@hotmail.com>
2022-09-21 01:15:56 -0300

    Classes e métodos abstratos

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
@@ -1,12 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
 
-public class Carro {
+public class Comprador {
     private String nome;
-    public static final double VELOCIDADE_LIMITE = 250;
-    public final Comprador COMPRADOR = new Comprador();
 
-    public final void imprime() {
-        System.out.println(this.nome);
+    @Override
+    public String toString() {
+        return "Comprador{" +
+                "nome='" + nome + '\'' +
+                '}';
     }
 
     public String getNome() {
@@ -16,4 +17,6 @@ public class Carro {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+
 }
