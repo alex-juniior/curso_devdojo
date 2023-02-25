c6a9e6182a8ebea5ebbfb7aadfee019acf964195
alex barroso <alexjunior651@hotmail.com>
2023-02-25 03:24:38 -0300

    CRUD, pt 02 - Delete

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZJcrud/repository/ProducerRepository.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZJcrud/repository/ProducerRepository.java
@@ -5,10 +5,7 @@ import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.conn.ConnectionFactory;
 import lombok.extern.log4j.Log4j2;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,6 +39,24 @@ public class ProducerRepository {
         String sql = "SELECT * FROM anime_store.producer where name like ?;";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setString(1, String.format("%%%s%%", name));
+        return ps;
+    }
+
+    public static void delete(int id) {
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPrepareStatementDelete(conn, id)) {
+            ps.execute();
+            log.info("Deleted producer '{}'from database, rows affected '{}' ", id);
+        } catch (SQLException e) {
+            log.error("Error while trying to insert producer '{} ", id, e);
+        }
+
+    }
+
+    private static PreparedStatement createPrepareStatementDelete(Connection conn, Integer id) throws SQLException {
+        String sql = "DELETE FROM `anime_store`.`producer` WHERE (`id` = ?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, id);
         return ps;
     }
 }
